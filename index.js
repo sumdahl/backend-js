a04fa4469ebf225b6ff95ed9ea5b02963401df72
sumdahl <sumirandahal46@gmail.com>
2024-02-07 18:29:30 +0545

    lil changes

--- index.js
+++ index.js
@@ -4,27 +4,31 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-const githubData = {
+const user= {
+    name: 'sumdahl',
+    age: '22',
+    isLearning: true
 }
 
-app.get('/github', (req, res) => {
-    res.json(githubData)
-})
 app.get('/', (req, res) => {
     res.send('Hello world!')
 })
 
 app.get('/twitter', (req, res) => {
-    res.send('@sumiran_dahal')
+    res.send('Please follow me at @sumiran_dahal')
 })
 
 app.get('/login', (req, res) => {
-    res.send('<h1>Please login at helloworld.com</h1>')
+    res.send('<h1>Please login at nepalstock.com for nepse update</h1>')
 })
 
 app.get('/youtube', (req, res) =>{
-    res.send('<h2>mitho chiya khana man lagyo')
+    res.send('<h2>mitho chiya khana man lagyo , kasto jaado abuii')
  })
+
+app.get('/github', (req, res) => {
+    res.json(githubData)
+})
  
 app.listen(process.env.PORT, () => {
     console.log(`Example app is listening at port ${process.env.PORT}`)
